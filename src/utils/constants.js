d085d76d570d6ac5924d00ef1bd896fd49590ae1
Igor Araujo <igor.dearaujo@live.com>
2020-06-15 23:49:41 -0300

    init constants

---
+++ src/utils/constants.js
@@ -0,0 +1,9 @@
+export const flightApiConstants = {
+    LANGUAGE: 'pt-BR',
+    MARKET: 'BR',
+    REQUEST_URL: 'skyscanner-skyscanner-flight-search-v1.p.rapidapi.com'
+}
+
+export const accomodationApiConstants = {
+    
+}
